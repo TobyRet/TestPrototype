e62d0ba483afebfabec9192b5cd0c5bb39d574df
Toby Retallick <toby.retallick@gmail.com>
2018-09-18 12:29:39 +0100

    Initial Commit
    - add MOJ front end library
    - Create sign on, dashboard, and allocations pages
    - Update .gitignore

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'HMPSS | Offender Management in Custody',
 
   // Default port that prototype runs on
   port: '3000',
--- app/routes.js
+++ app/routes.js
@@ -2,5 +2,18 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+router.post('/sign-in', function(req, res) {
+  res.redirect('/allocations')
+})
+
+router.get('/prisoner/:id', function(req, res) {
+  let data = {
+    prisoner_name: 'Toby',
+    dob: '24/01/1980',
+    nationality: 'New Zealander'
+  }
+
+  res.render('prisoner', data)
+})
 
 module.exports = router
